90e3bc5709eca56d1f6ed3f85e493057013c9472
huggeri <katq228@gmail.com>
2017-05-28 18:56:17 +0700

    Create main_Long_int3.cpp

---
+++ main_Long_int3.cpp
@@ -0,0 +1,182 @@
+//(интерфейс)
+#include "Long_int3.h"
+
+char *menu_action(int key)
+{
+	char *action = " WHAT? ";
+	switch (key)
+	{
+	case 1:
+	{
+		action = " * ";
+		break;
+	}
+	case 2:
+	{
+		action = " / ";
+		break;
+	}
+	case 3:
+	{
+		action = " + ";
+		break;
+	}
+	case 4:
+	{
+		action = " - ";
+		break;
+	}
+	case 5:
+	{
+		action = " = ";
+		break;
+	}
+	}
+	return action;
+}
+
+char *menu(int key)
+{
+	char *text = "Error!";
+	switch (key)
+	{
+	case 1:
+	{
+		text = "Enter values:";
+		break;
+	}
+	case 2:
+	{
+		text = "Select an action:\n"
+			"1 - Summand\n2 - Division\n3 - Summary\n4 - Substraction\n5 - Get number by digit\n0 - Exit";
+		break;
+	}
+	case 3:
+	{
+		text = "Enter digit and object number:";
+		break;
+	}
+	case 4:
+	{
+		text = "Total objects: ";
+		break;
+	}
+	case 5:
+	{
+		text = "Error: unable to write to Newfile.txt ";
+		break;
+	}
+	}
+	return text;
+}
+
+void main()
+{
+	bool flag = true;
+
+	while (flag != !flag)
+	{
+		int menu_key = 0;
+		cout << menu(1) << endl;
+		Unsigned_fractional_number val1;
+		Unsigned_fractional_number val2;
+		Unsigned_fractional_number result;
+		cin >> val1;
+		cin >> val2;
+		cout << menu(2) << endl;
+		cin >> menu_key;
+
+		switch (menu_key)//вызываем функции
+		{
+		case 1://умножение
+		{
+			result = val1 * val2;
+			cout << val1 << menu_action(menu_key) << val2 << menu_action(5) << result << "\t";
+			cout << menu(4) << Long_int::get_count_objects() << endl;
+			break;
+		}
+		case 2://деление
+		{
+			result = val1 / val2;
+			cout << val1 << menu_action(menu_key) << val2 << menu_action(5) << result << "\t";
+			cout << menu(4) << Long_int::get_count_objects() << endl;
+			break;
+		}
+		case 3://сложение
+		{
+			result = val1 + val2;
+			cout << val1 << menu_action(menu_key) << val2 << menu_action(5) << result << "\t";
+			cout << menu(4) << Long_int::get_count_objects() << endl;
+			break;
+		}
+		case 4://вычитание
+		{
+			result = val1 - val2;
+			cout << val1 << menu_action(menu_key) << val2 << menu_action(5) << result << "\t";
+			cout << menu(4) << Long_int::get_count_objects() << endl;
+			break;
+		}
+		case 5://вывод цифры
+		{
+			int index = -1, key = 0;
+			cout << menu(3) << endl;
+			cin >> index;
+			cin >> key;
+			switch (key)
+			{
+			case 1:
+			{
+				index = val1.get_digit(index - 1);
+				break;
+			}
+			case 2:
+			{
+				index = val2.get_digit(index - 1);
+				break;
+			}
+			default:
+				index = -1;
+			}
+			if (index >= 0)
+			{
+				cout << menu_action(menu_key) << index << endl;
+			}
+			else
+				cout << menu(0) << endl;
+			break;
+		}
+		//case 6:
+		//{
+		//	ofstream f("Newfile111222.txt", ios::out | ios::trunc);
+		//	if (!f)
+		//	{
+		//		cerr << menu(5) << endl;
+		//		exit(1);
+		//	}
+		//	f << val1;
+		//	f.close();
+		//	break;
+		//}
+		//case 7:
+		//{
+		//	ifstream f("Newfile111222.txt");
+		//	if (!f)
+		//	{
+		//		cerr << menu(5) << endl;
+		//		exit(1);
+		//	}
+		//	f >> val2;
+		//	f.close();
+		//	cout << val2 << endl;
+		//	break;
+		//}
+		case 0:
+		{
+			cout << menu(4) << Long_int::get_count_objects() << endl;
+			exit(0);
+		}
+		default:
+			cout << menu(0) << endl;
+		}
+	}
+}
